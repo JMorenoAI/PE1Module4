a6f27db942191d4526cff8e9386d2ca73e5f5461
hongj <jomore01@wsc.edu>
2023-02-16 10:33:46 -0600

    Added 3 files to the repo

---
+++ student_maintenance.py
@@ -0,0 +1,10 @@
+students = {}
+
+# Define a function to add a new student to the dictionary
+def add_student():
+    # Ask the user for an id, first name, and last name
+    id= input("Enter the student id: ")
+    first_name = input("Enter the first name: ")
+    last_name = input("Enter the last name: ")
+    # Validate the id, first name, and last name
+
