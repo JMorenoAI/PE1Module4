3e7c2c1879bdfbe1fc6992c089fc47879f6229ff
hongj <jomore01@wsc.edu>
2023-02-23 11:03:11 -0600

    Fixed some codes

--- student_maintenance.py
+++ student_maintenance.py
@@ -6,7 +6,6 @@ __version__ = '1.0'
 __date__ = '2023.02.20'
 __status__ = 'Release'
 
-
 # This is a Python program for managing student information. You can add, list, update, and delete a student.
 
 # Data validation used to verify whether the user entered a valid data input
@@ -20,17 +19,11 @@ next_student_id = 1
 
 # Function to add a new student
 def add_student(students, next_student_id):
-    # Get student ID
-    student_id = dv.get_num("Enter student ID: ")
-    # Check if student ID is already in use
-    if student_id in students:
-        print("Student ID already exists.")
-        return
     # Get first name and last name
     first_name = dv.get_string("Enter first name: ")
     last_name = dv.get_string("Enter last name: ")
     # Add student to dictionary
-    students[student_id] = {"first name": first_name, "last name": last_name}
+    students[next_student_id] = {"first name": first_name, "last name": last_name}
     print("Student added successfully.")
 
 
@@ -51,12 +44,17 @@ def list_student():
 
 # Function to update a student
 def update_student():
+    # Check if there are students to update their information.
+    if len(students) == 0:
+        print("There are no students to update")
+        return
     # Get student ID
     student_id = dv.get_num("Enter student ID: ")
     # Check if student ID exists
     if student_id not in students:
         print("Student ID does not exist.")
         return
+
     # Get first name and last name, if user chooses to update
     first_name = input("Enter first name: ").students[student_id]["first name"]
     last_name = input("Enter last name: ").students[student_id]["last name"]
@@ -68,14 +66,19 @@ def update_student():
 
 # Function to delete a student
 def delete_student():
+    # Check if there are students to delete
+    if len(students) == 0:
+        print("There are no students to delete.")
+        return
     # Get student ID
     student_id = dv.get_num("Enter student ID: ")
     # Check if student ID exists
     if student_id not in students:
         print("Student ID does not exist.")
         return
+
     # Confirm delete action
-    confirm = input("Are you sure you want to delete this student? (y/n): ").strip().lower()
+    confirm = input("Are you sure you want to delete this student? (y/n): ")
     if confirm == "y":
         # Remove student from dictionary
         del students[student_id]
@@ -83,26 +86,3 @@ def delete_student():
     else:
         print("Action canceled.")
 
-
-# Main program loop
-while True:
-    print("\nStudent Maintenance")
-    print("1. List a student")
-    print("2. Add a student")
-    print("3. Update a student")
-    print("4. Delete a student")
-    print("5. Quit")
-    choice = dv.get_num("Enter your choice: ")
-
-    if choice == 1:
-        list_student()
-    elif choice == 2:
-        add_student()
-    elif choice == 3:
-        update_student()
-    elif choice == 4:
-        delete_student()
-    elif choice == 5:
-        break
-    else:
-        print("Invalid choice.")
